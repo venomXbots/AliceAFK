1f6bc3c5dd8a8705f8001502d344a3e1caaf2b62
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-03 04:08:33 +0530

    Update helpers.py

--- Alice/helpers.py
+++ Alice/helpers.py
@@ -1,9 +1,11 @@
 #
 # Copyright (C) 2021-2022 by TeamInflex@Github, < https://github.com/TeamInflex >.
 #
-# This file is part of < https://github.com/TeamInflex/InflexAFKBot > project,
+# Copyright (C) 2021-2022 by venomXbots@Github, < https://github.com/TeamInflex >.
+#
+# This file is part of < https://github.com/venomXbots/AliceAFK > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamInflex/InflexAFKBot/blob/master/LICENSE >
+# Please see < https://github.com/venomXbots/AliceAFK/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -11,8 +13,8 @@ import asyncio
 
 from typing import Union
 from datetime import datetime, timedelta
-from Inflex import cleanmode, app, botname
-from Inflex.database import is_cleanmode_on
+from Alice import cleanmode, app, botname
+from Alice.database import is_cleanmode_on
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton
 
@@ -76,66 +78,37 @@ asyncio.create_task(auto_clean())
 
 
 RANDOM = [
-    "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
-    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
-    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
-    "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
-    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
-    "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
-    "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
-    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
-    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
-    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
-    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
-    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
-    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
-    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
-    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
-    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
-    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
-    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
-    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
-    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
-    "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
-    "https://telegra.ph//file/07bb5f805178ec24871d3.jpg"
+    "https://telegra.ph/file/b528d894684d8583b00aa.jpg",
 ]
 
 
-HELP_TEXT = f"""Welcome to {botname}'s Help Section.
+HELP_TEXT = f"""ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {botname}'s ʜᴇʟᴘ sᴇᴄᴛɪᴏɴ.
 
-- When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
+- ᴡʜᴇɴ sᴏᴍᴇᴏɴᴇ ᴍᴇɴᴛɪᴏɴs ʏᴏᴜ ɪɴ ᴀ ᴄʜᴀᴛ, ᴛʜᴇ ᴜsᴇʀ ᴡɪʟʟ ʙᴇ ɴᴏᴛɪғɪᴇᴅ ʏᴏᴜ ᴀʀᴇ ᴀғᴋ. ʏᴏᴜ ᴄᴀɴ ᴇᴠᴇɴ ᴘʀᴏᴠɪᴅᴇ ᴀ ʀᴇᴀsᴏɴ ғᴏʀ ɢᴏɪɴɢ ᴀғᴋ, ᴡʜɪᴄʜ ᴡɪʟʟ ʙᴇ ᴘʀᴏᴠɪᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴜsᴇʀ ᴀs ᴡᴇʟʟ.
 
 
-/afk - This will set you offline.
+/afk - ᴛʜɪs ᴡɪʟʟ sᴇᴛ ʏᴏᴜ ᴏғғʟɪɴᴇ.
 
-/afk [Reason] - This will set you offline with a reason.
+/afk [Reason] - ᴛʜɪs ᴡɪʟʟ sᴇᴛ ʏᴏᴜ ᴏғғʟɪɴᴇ ᴡɪᴛʜ ᴀ ʀᴇᴀsᴏɴ.
 
-/afk [Replied to a Sticker/Photo] - This will set you offline with an image or sticker.
+/afk [Replied to a Sticker/Photo] - ᴛʜɪs ᴡɪʟʟ sᴇᴛ ʏᴏᴜ ᴏғғʟɪɴᴇ ᴡɪᴛʜ ᴀɴ ɪᴍᴀɢᴇ ᴏʀ sᴛɪᴄᴋᴇʀ.
 
-/afk [Replied to a Sticker/Photo] [Reason] - This will set you afk with an image and reason both.
+/afk [Replied to a Sticker/Photo] [Reason] - ᴛʜɪs ᴡɪʟʟ sᴇᴛ ʏᴏᴜ ᴀғᴋ ᴡɪᴛʜ ᴀɴ ɪᴍᴀɢᴇ ᴀɴᴅ ʀᴇᴀsᴏɴ ʙᴏᴛʜ.
 
-/settings - To change or edit basic settings of AFK Bot.
+/settings - ᴛᴏ ᴄʜᴀɴɢᴇ ᴏʀ ᴇᴅɪᴛ ʙᴀsɪᴄ sᴇᴛᴛɪɴɢs ᴏғ ᴀғᴋ ʙᴏᴛ.
 """
 
 def settings_markup(status: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="🔄 Clean Mode", callback_data="cleanmode_answer"),
+            InlineKeyboardButton(text="🔄 ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="cleanmode_answer"),
             InlineKeyboardButton(
                 text="✅ Enabled" if status == True else "❌ Disabled",
                 callback_data="CLEANMODE",
             ),
         ],
         [
-            InlineKeyboardButton(text="🗑 Close Menu", callback_data="close"),
+            InlineKeyboardButton(text="🗑 ᴄʟᴏsᴇ ᴍᴇɴᴜ", callback_data="close"),
         ],
     ]
     return buttons
