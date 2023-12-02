cbf7a67ee20c0f41c54aa42431f2fc21e4c792c4
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-03 02:38:58 +0530

    Update chats.py

--- Alice/Database/chats.py
+++ Alice/Database/chats.py
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2021-2022 by TeamInflex@Github, < https://github.com/TeamInflex >.
+# Copyright (C) 2021-2022 by venomXbots@Github, < https://github.com/TeamInflex >.
 #
-# This file is part of < https://github.com/TeamInflex/InflexAFKBot > project,
+# This file is part of < https://github.com/venomXbots/AliceAFK > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamInflex/InflexAFKBot/blob/master/LICENSE >
+# Please see < https://github.com/venomXbots/AliceAFK/blob/master/LICENSE >
 #
 # All rights reserved.
 #
 
-from Inflex import db
+from Alice import db
 
 chatsdb = db.chatsdb
 
