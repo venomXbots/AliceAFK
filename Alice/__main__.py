3ee5d5c27d1d0a085a582f3d4c955a5ec84860c2
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-03 03:45:52 +0530

    Update and rename __main__ (2).py to __main__.py

--- Alice/__main__.py
+++ Alice/__main__.py
@@ -1,19 +1,18 @@
 #
-# Copyright (C) 2021-2022 by TeamInflex@Github, < https://github.com/TeamInflex >.
+# Copyright (C) 2021-2022 by venomXbots@Github, < https://github.com/TeamInflex >.
 #
-# This file is part of < https://github.com/TeamInflex/InflexAFKBot > project,
+# This file is part of < https://github.com/venomXbots/AliceAFK > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamInflex/InflexAFKBot/blob/master/LICENSE >
+# Please see < https://github.com/venomXbots/AliceAFK/blob/master/LICENSE >
 #
 # All rights reserved.
-
 
 import asyncio
 import importlib
 
 from pyrogram import idle
 
-from Inflex.modules import ALL_MODULES
+from Alice.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
 
@@ -21,7 +20,7 @@ loop = asyncio.get_event_loop()
 async def initiate_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("Inflex.modules." + all_module)
-    print("Started Inflex AFK Bot.")
+    print("Started Alice AFK Bot.")
     await idle()
     print("GoodBye! Stopping Bot")
 
