1aac908505b3697c66cfa600b42d89bda43bb3e8
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-03 02:37:15 +0530

    Create __init__.py

---
+++ Alice/Database/__init__.py
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021-2022 by venomXbots@Github, < https://github.com/TeamInflex >.
+#
+# This file is part of < https://github.com/venomXbots/AliceAFK > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/venomXbots/AliceAFK/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+
+from .chats import *
+from .users import *
+from .cleanmode import *
+#
+
+
+from .chats import *
+from .users import *
+from .cleanmode import *
