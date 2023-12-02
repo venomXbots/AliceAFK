3bf1d0ec2a3af0a23b22137d9fed90a7e4c864b3
venomXbots <152503093+venomXbots@users.noreply.github.com>
2023-12-03 03:43:25 +0530

    Update __init__.py

--- Alice/__init__ (3).py
+++ Alice/__init__ (3).py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamInflex@Github, < https://github.com/TeamInflex >.
+# Copyright (C) 2021-2022 by venomXbots@Github, < https://github.com/TeamInflex >.
 #
-# This file is part of < https://github.com/TeamInflex/InflexAFKBot > project,
+# This file is part of < https://github.com/venomXbots/AliceAFK > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamInflex/InflexAFKBot/blob/master/LICENSE >
+# Please see < https://github.com/venomXbots/AliceAFK/blob/master/LICENSE >
 #
 # All rights reserved.
 
